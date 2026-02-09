9d66dbdc4b0403c7f5072808f7d6caab5cdc2ae5
theplaceincan <abrahatatsbeha@gmail.com>
2026-02-08 21:22:28 -0800

    updated navbar contact info

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,9 +6,9 @@ export default function Navbar() {
       <h1 className={styles.name}>Abraha Atsbeha</h1>
       <p className={styles.subHeader}>Computer Science · University of Nevada, Las Vegas</p>
       <div className={styles.links}>
-        <a href="mailto:atsbeha@unlv.nevada.edu">Email</a>
-        <a href="https://github.com/atsbeha">GitHub</a>
-        <a href="https://linkedin.com/in/atsbeha">LinkedIn</a>
+        <a href="mailto:aatsbeha35@gmail.com">Email</a>
+        <a href="https://github.com/theplaceincan">GitHub</a>
+        <a href="https://linkedin.com/in/abrahaatsbeha">LinkedIn</a>
       </div>
     </header>
   );
